f0333472566c4fdeb84c2cc270252cee30a27d2a
Chris Robertson <cjrobertson@worldorderstudios.com>
2020-02-01 18:06:25 -0800

    Upload translation app

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py
+
+from Translate import process_text
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return process_text("Hello world", "en", "es")
+
+
+# Accept a POST request to translate text using AWS Translate.
+@app.route('/translate_text', methods=["POST"])
+def translate_text():
+    data = request.get_json()
+    translated_text = process_text(data['textToTranslate'], "en", "es")
+    return jsonify({'textToTranslate': translated_text})
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True)
