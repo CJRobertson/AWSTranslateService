2c83c9f58c8328813ecf308aa7c2b02970a9a20b
Chris Robertson <cjrobertson@worldorderstudios.com>
2020-02-03 02:11:20 -0800

    Update translate.py
    Removed duplicate 'process_text' function.

--- app/translateservice/translate.py
+++ app/translateservice/translate.py
@@ -8,13 +8,6 @@ import os
 translate = boto3.client(service_name='translate', region_name='us-west-2', use_ssl=True)
 
 
-# Process text that needs to be translated by AWS Translate.
-#def process_text(text_to_translate, source_lang, target_lang):
-#    result = translate.translate_text(Text=text_to_translate, SourceLanguageCode=source_lang,
-#                                      TargetLanguageCode=target_lang)
-#    return result.get('TranslatedText')
-
-
 class TranslateText(object):
     def __init__(self) -> None:
         try:
